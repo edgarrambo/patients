23482380d0ba9b61f853e9aa245ccbce901cfb84
edgarrambo <ragde956@gmail.com>
2019-11-23 05:17:57 +0000

    added database

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,19 @@ class PatientsController < ApplicationController
 	def index
 		@patients = Patient.all
 	end
+
+	def new
+		@patient = Patient.new
+	end
+
+	def create 
+		Patient.create(patient_params)
+		redirect_to root_path
+end
+
+private
+
+def patient_params
+	params.require(:patient).permit(:name, :age, :gender, :address, :phone_number, :email)
+end
 end
